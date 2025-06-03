17ab880422a0ccaef710d0a6a1d6f0a550cf5bcb
ifknot <ifknot@hotmail.com>
2025-06-03 13:37:33 +0100

    Create composite.h

---
+++ src/TREE/composite_types.h
@@ -0,0 +1,21 @@
+//The GoF (Gang of Four) Composite Design Pattern 
+
+// Forward declare opaque pointer types
+typedef struct component component_t;
+typedef struct composite composite_t;
+
+// Function pointer types for public interface
+typedef void (*component_operation_fn)(component_t *self);
+typedef void (*component_add_fn)(component_t *self, component_t *child);
+
+// Public interface functions
+component_t* component_create(void);
+
+void component_destroy(component_t *self);
+
+void component_operate(component_t *self);
+
+// Composite-specific interface
+composite_t* composite_create(void);
+
+void composite_add(composite_t *comp, component_t *child);
